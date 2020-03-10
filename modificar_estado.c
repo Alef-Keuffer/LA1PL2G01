7836ddf9132bdca20188da8b102902177be48984
Alef-Keuffer <56977642+Alef-Keuffer@users.noreply.github.com>
2020-03-10 21:15:59 +0000

    Update modificar_estado.c

--- modificar_estado.c
+++ modificar_estado.c
@@ -1,17 +1,17 @@
 #include "dados.h"
 
 void atualizar_tab(ESTADO *e, COORDENADA c){
-    for (int row = e.tamanho_tabuleiro; row > 0; --row)
-        for (int col = 1; col <= e.tamanho_tabuleiro; ++col)
-            if (row==c.linha && col==c.coluna) e.tab[row][col] = c;
+    for (int row = (*e).tamanho_tabuleiro; row > 0; --row)
+        for (int col = 1; col <= (*e).tamanho_tabuleiro; ++col)
+            if (row==c.linha-1 && col==c.coluna-1) (*e).tab[row-1][col-1] = c;
 }
 void atualizar_ultima_jogada(ESTADO *e, COORDENADA c){
-    e.ultima_jogada = c;
+    (*e).ultima_jogada = c;
 }
 void atualizar_num_jogadas(ESTADO *e){
-    e.num_jogadas = e.num_jogadas +1
+    (*e).num_jogadas = (*e).num_jogadas +1
 }
 void atualizar_jogador_atual(ESTADO *e){
-    if (e.jogador_atual == 1) e.jogador_atual = 2;
-    else e.jogador_atual = 1;
+    if ((*e).jogador_atual == 1) (*e).jogador_atual = 2;
+    else (*e).jogador_atual = 1;
 }
