054b5e81449ece25586140a31fe19335a4383fd6
Alef-Keuffer <56977642+Alef-Keuffer@users.noreply.github.com>
2020-03-10 20:41:07 +0000

    Update dados.h
    Tenho que fazer modificações quando decidires como o tabuleiro for inicializado, talvez as definições de  'int tamanho_tabuleiro'
    e 'CASA tab[tamanho_tabuleiro][tamanho_tabuleiro]' não sejam necessárias.

--- dados.h
+++ dados.h
@@ -1,24 +1,24 @@
 #ifndef DADOS_H_
 #define DADOS_H_
 
+// Estruturas de dados (devem ser colocadas no módulo correto da camada dos dados)
 typedef enum {VAZIO, BRANCA, PRETA} CASA;
-
 typedef struct {
-int coluna;
+    int coluna;
     int linha;
 } COORDENADA;
-
 typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
-
 typedef JOGADA JOGADAS[32];
-
 typedef struct {
-CASA tab[8][8]; COORDENADA ultima_jogada; JOGADAS jogadas;
-int num_jogadas;
-int jogador_atual;
+    int tamanho_tabuleiro;
+    CASA tab[tamanho_tabuleiro][tamanho_tabuleiro];
+    COORDENADA ultima_jogada;
+    JOGADAS jogadas;
+    int num_jogadas;
+    int jogador_atual;
 } ESTADO;
 
 #endif
