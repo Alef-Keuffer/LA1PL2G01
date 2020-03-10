78a96772657ad3e41324f89d07d23dace7f8d906
Alef-Keuffer <56977642+Alef-Keuffer@users.noreply.github.com>
2020-03-10 20:44:56 +0000

    Create modificar_estado.h

---
+++ modificar_estado.h
@@ -0,0 +1,9 @@
+#ifndef MODIFICAR_ESTADO_H
+#define MODIFICAR_ESTADO_H
+
+void atualizar_tab(ESTADO *e, COORDENADA c){};
+void atualizar_ultima_jogada(ESTADO *e, COORDENADA c);
+void atualizar_num_jogadas(ESTADO *e);
+void atualizar_jogador_atual(ESTADO *e);
+
+#endif //MODIFICAR_ESTADO_H
