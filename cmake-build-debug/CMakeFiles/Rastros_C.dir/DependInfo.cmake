c9cf98f4d7073c3ba8a294007fc1add960858b13
Alef Keuffer <alefkeuffer@gmail.com>
2020-03-11 17:55:41 +0000

    Deixando integrado com o CLion.
    Signed-off-by: Alef Keuffer <alefkeuffer@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/Rastros_C.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/alefk/Documents/Rastros_C/dados.c" "C:/Users/alefk/Documents/Rastros_C/cmake-build-debug/CMakeFiles/Rastros_C.dir/dados.c.obj"
+  "C:/Users/alefk/Documents/Rastros_C/interface.c" "C:/Users/alefk/Documents/Rastros_C/cmake-build-debug/CMakeFiles/Rastros_C.dir/interface.c.obj"
+  "C:/Users/alefk/Documents/Rastros_C/logica.c" "C:/Users/alefk/Documents/Rastros_C/cmake-build-debug/CMakeFiles/Rastros_C.dir/logica.c.obj"
+  "C:/Users/alefk/Documents/Rastros_C/main.c" "C:/Users/alefk/Documents/Rastros_C/cmake-build-debug/CMakeFiles/Rastros_C.dir/main.c.obj"
+  "C:/Users/alefk/Documents/Rastros_C/modificar_estado.c" "C:/Users/alefk/Documents/Rastros_C/cmake-build-debug/CMakeFiles/Rastros_C.dir/modificar_estado.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Rastros_C.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rastros_C.dir/dados.c.obj"
+  "CMakeFiles/Rastros_C.dir/interface.c.obj"
+  "CMakeFiles/Rastros_C.dir/logica.c.obj"
+  "CMakeFiles/Rastros_C.dir/main.c.obj"
+  "CMakeFiles/Rastros_C.dir/modificar_estado.c.obj"
+  "Rastros_C.exe"
+  "Rastros_C.exe.manifest"
+  "Rastros_C.pdb"
+  "libRastros_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rastros_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
