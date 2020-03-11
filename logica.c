aec79abb9ecb7f1b24c2aa27a26e873c960fd72f
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-03-11 15:33:00 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include "modificar_estado.h"
 #include "dados.h"
-#include "interface.h"
 
 // Função que deve ser completada e colocada na camada da lógica do programa
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %c %d\n", c.coluna, c.linha);
-    c.coluna = c.coluna -64;
     atualizar_tab(e, c);
     atualizar_ultima_jogada(e, c);
-    if(e->jogador_atual == 2) atualizar_num_jogadas(e);
+    atualizar_num_jogadas(e);
     atualizar_jogador_atual(e);
     mostrar_tabuleiro(e);
     return 1;
