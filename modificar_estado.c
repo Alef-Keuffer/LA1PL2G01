f5339d744f2ab5db77145068af40a221fa7f26c2
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-03-11 15:34:00 +0000

    Update modificar_estado.c

--- modificar_estado.c
+++ modificar_estado.c
@@ -1,17 +1,20 @@
 #include "dados.h"
 
 void atualizar_tab(ESTADO *e, COORDENADA c){
-    for (int row = e.tamanho_tabuleiro; row > 0; --row)
-        for (int col = 1; col <= e.tamanho_tabuleiro; ++col)
-            if (row==c.linha && col==c.coluna) e.tab[row][col] = c;
+    (*e).tab[(*e).ultima_jogada.linha][(*e).ultima_jogada.coluna] = PRETA;
+    (*e).tab[c.linha][c.coluna] = BRANCA;
 }
 void atualizar_ultima_jogada(ESTADO *e, COORDENADA c){
-    e.ultima_jogada = c;
+    (*e).ultima_jogada = c;
 }
 void atualizar_num_jogadas(ESTADO *e){
     if ((*e).jogador_atual == 2) (*e).num_jogadas = (*e).num_jogadas +1;
 }
+void atualizar_jogadas (ESTADO *e, COORDENADA c){
+    if ((*e).jogador_atual == 1) (*e).jogadas[(*e).num_jogadas].jogador1 = c;
+    else (*e).jogadas[(*e).num_jogadas].jogador2 = c;
+}
 void atualizar_jogador_atual(ESTADO *e){
-    if (e.jogador_atual == 1) e.jogador_atual = 2;
-    else e.jogador_atual = 1;
+    if ((*e).jogador_atual == 1) (*e).jogador_atual = 2;
+    else (*e).jogador_atual = 1;
 }
