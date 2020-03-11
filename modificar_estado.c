f633b1f740bc5bfca6db34fbc1a4142e022d5530
Alef-Keuffer <56977642+Alef-Keuffer@users.noreply.github.com>
2020-03-11 15:07:05 +0000

    Update modificar_estado.c

--- modificar_estado.c
+++ modificar_estado.c
@@ -8,7 +8,7 @@ void atualizar_ultima_jogada(ESTADO *e, COORDENADA c){
     (*e).ultima_jogada = c;
 }
 void atualizar_num_jogadas(ESTADO *e){
-    (*e).num_jogadas = (*e).num_jogadas +1;
+    if ((*e).jogador_atual == 2) (*e).num_jogadas = (*e).num_jogadas +1;
 }
 void atualizar_jogadas (ESTADO *e, COORDENADA c){
     if ((*e).jogador_atual == 1) (*e).jogadas[(*e).num_jogadas].jogador1 = c;
