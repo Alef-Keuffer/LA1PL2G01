9996d4c5d372015875300f2579af33d26be9d43c
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-03-11 15:37:06 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -4,7 +4,7 @@
 
 // Função que deve ser completada e colocada na camada da lógica do programa
 int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %c %d\n", c.coluna, c.linha);
+    printf("jogar %c %d\n", c.coluna+'a', c.linha+1);
     atualizar_tab(e, c);
     atualizar_ultima_jogada(e, c);
     atualizar_num_jogadas(e);
