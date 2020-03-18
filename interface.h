ba549173d9ac8bb62c852db9f0df4f2c7919a5f7
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-03-18 15:50:28 +0000

    Implementação do prompt.

--- interface.h
+++ interface.h
@@ -1,7 +1,10 @@
 #ifndef INTERFACE_H_
 #define INTERFACE_H_
+#include "dados.h"
 
 void mostrar_tabuleiro(ESTADO *e);
-int interpretador(ESTADO *e);
+void prompt(ESTADO *e, int num_comandos);
+int interpretador(ESTADO *e, int num_comandos);
+void movs (ESTADO *e);
 
 #endif
