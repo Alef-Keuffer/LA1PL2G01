cd4b01b7c2938377cc91d722c6b7bbf29643ad13
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-03-18 15:53:01 +0000

    Implementação de obter_ultimajogada.

--- acessar_estado.h
+++ acessar_estado.h
@@ -1,8 +1,11 @@
 #ifndef RASTROS_C_ACESSAR_ESTADO_H
 #define RASTROS_C_ACESSAR_ESTADO_H
+#include "dados.h"
 
 int obter_casa (ESTADO *e, int row, int col);
 int JogadorAtual (ESTADO *e);
 int obter_subcoordenadas (ESTADO *e, int jogada, int jogador, int info);
 int NumJogadas(ESTADO *e);
+COORDENADA obter_ultimajogada(ESTADO *e);
+
 #endif //RASTROS_C_ACESSAR_ESTADO_H
