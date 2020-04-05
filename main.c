7f1d76b045e6f60882f4511aa2e070aa583f2dfa
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-05 14:00:53 +0100

    Estado para a função pos.

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "interface.h"
 
 int main() {
-    int num_comandos = 0;
+    int num_comandos = 0, state = 0;
     ESTADO *e = inicializar_estado();
-    interpretador(e, num_comandos);
+    interpretador(e, num_comandos, state);
     return 0;
 }
