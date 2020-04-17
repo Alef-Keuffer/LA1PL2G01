b55a19415b62a5813fe03de6c5fca9312aa85d47
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-17 17:49:23 +0100

    Função jog + funções auxiliares.

--- Camadas/logica/logica.h
+++ Camadas/logica/logica.h
@@ -1,10 +1,16 @@
 #ifndef RASTROS_LOGICA_H
 #define RASTROS_LOGICA_H
-#include "Camadas/dados/dados.h"
+#include "dados.h"
 
 int jogar(ESTADO *e, COORDENADA c);
 int jogada_valida(ESTADO *e, COORDENADA c);
 int fim_de_jogo(ESTADO *e);
 void pos(ESTADO *e, int position, int state);
+int coord_to_int(COORDENADA c);
+COORDENADA int_to_coord(int x);
+int avaliar_vitoria(ESTADO *e);
+int desfazer_ultjogada(ESTADO *e);
+int minMax(ESTADO *e);
+void jog(ESTADO *e);
 
 #endif //RASTROS_LOGICA_H
