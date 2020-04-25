9e451fd6084043c75cbbadac72d06f832f95da33
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-25 18:56:49 +0100

    Novas funções auxiliares e mudança na função mostrar_tabuleiro.

--- Camadas/interface/interface.h
+++ Camadas/interface/interface.h
@@ -1,10 +1,12 @@
 #ifndef RASTROS_INTERFACE_H
 #define RASTROS_INTERFACE_H
+#include <stdio.h>
 #include "Camadas/dados/dados.h"
 
-void mostrar_tabuleiro(ESTADO *e);
+void imprimir_tabuleiro(ESTADO *e, FILE *file);
 void prompt(ESTADO *e, int num_comandos);
 int interpretador(ESTADO *e, int num_comandos, int state);
-void movs (ESTADO *e);
+void imprime_movs1(FILE *file, int i, int c1, int r1, int c2, int r2);
+void imprime_movs2(FILE *file, int i, char c1, char r1);
 
 #endif //RASTROS_INTERFACE_H
