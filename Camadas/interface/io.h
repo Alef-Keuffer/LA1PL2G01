6f60525b54e9d2f5a2459bcfb0c64d4d06d0dd28
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-29 15:26:38 +0100

    Update io.h

--- Camadas/interface/io.h
+++ Camadas/interface/io.h
@@ -1,9 +1,9 @@
 #ifndef RASTROS_IO_H
 #define RASTROS_IO_H
 #include <stdio.h>
-#include "dados.h"
-#include "logica.h"
-#include "auxiliares.h"
+#include "Camadas/dados/dados.h"
+#include "Camadas/logica/logica.h"
+#include "Camadas/logica/auxiliares.h"
 
 void gr (ESTADO *e, char *file_path);
 void ler (ESTADO *e, char *filename);
