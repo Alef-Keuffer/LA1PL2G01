5904a9c758c7649d283ec9d30780442e604bfafa
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-29 15:28:44 +0100

    Melhorias na modularização.

--- Camadas/interface/interface.h
+++ Camadas/interface/interface.h
@@ -1,12 +1,17 @@
 #ifndef RASTROS_INTERFACE_H
 #define RASTROS_INTERFACE_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "Camadas/dados/dados.h"
+#include "Camadas/logica/logica.h"
+#include "Camadas/interface/io.h"
 
-void imprimir_tabuleiro(ESTADO *e, FILE *file);
-void prompt(ESTADO *e, int num_comandos);
+void imprimir_prompt(int num_comandos, int jog, int num);
+void mensagem_fimdejogo(ESTADO *e);
 int interpretador(ESTADO *e, int num_comandos, int state);
+void imprimir_char(int c, FILE *file);
 void imprime_movs1(FILE *file, int i, int c1, int r1, int c2, int r2);
-void imprime_movs2(FILE *file, int i, char c1, char r1);
+void imprime_movs2(FILE *file, int i, int c1, int r1);
 
 #endif //RASTROS_INTERFACE_H
