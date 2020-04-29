ab31a7b44d85357bd5618355f2ebea8539d82ba3
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-29 15:27:22 +0100

    Melhorias na modularização.

--- Camadas/interface/interface.c
+++ Camadas/interface/interface.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Camadas/dados/dados.h"
-#include "Camadas/logica/logica.h"
-#include "Camadas/dados/acessar_estado.h"
-#include "Camadas/logica/io.h"
+#include "interface.h"
 
 #define BUF_SIZE 1024
-void imprimir_tabuleiro(ESTADO *e, FILE *file);
 
-void prompt(ESTADO *e, int num_comandos){
-    int num = NumJogadas(e) + 1, jog = JogadorAtual(e);
+void imprimir_prompt(int num_comandos, int jog, int num){
     printf("# %02d PL%d (%d)>", num_comandos, jog, num);
+}
+
+void mensagem_fimdejogo(ESTADO *e){
+    if(fim_de_jogo(e)) printf("Fim de jogo! O jogador %d venceu.\n", fim_de_jogo(e));
 }
 
 int interpretador(ESTADO *e, int num_comandos, int state){
     char linha[BUF_SIZE], filename[BUF_SIZE], command[5];
-    char col[2], lin[2], *str1, *str2, *str3;
+    char col[2], lin[2];
     int position;
 
-    if(fim_de_jogo(e)) printf("Fim de jogo! O jogador %d venceu.\n", fim_de_jogo(e));
+    mensagem_fimdejogo(e);
     prompt(e, num_comandos);
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
@@ -28,7 +24,7 @@ int interpretador(ESTADO *e, int num_comandos, int state){
         if(jogada_valida(e, coord)){
             jogar(e, coord);
             printf("jogar %c %c\n", coord.coluna +'a', coord.linha +'1');
-            imprimir_tabuleiro(e, NULL);
+            mostrar_tabuleiro(e, NULL);
             state = 0;
         }
         else{
@@ -45,30 +41,24 @@ int interpretador(ESTADO *e, int num_comandos, int state){
     if(strlen(linha) == 5 && sscanf(linha, "%*[m]%*[o]%*[v]%*[s]%c", command) == 1) movs(e, NULL);
     if(sscanf(linha, "%*[p]%*[o]%*[s] %d", &position) == 1) {
         pos(e, position, state);
-        imprimir_tabuleiro(e, NULL);
+        mostrar_tabuleiro(e, NULL);
         state = 1;
     }
     if(!fim_de_jogo(e) && strlen(linha) == 4 && sscanf(linha, "%*[j]%*[o]%*[g]%c", command) == 1){
         jog(e);
-        imprimir_tabuleiro(e, NULL);
+        mostrar_tabuleiro(e, NULL);
     }
     if(!fim_de_jogo(e) && strlen(linha) == 5 && sscanf(linha, "%*[j]%*[o]%*[g]%*[2]%c", command) == 1){
         jog2(e);
-        imprimir_tabuleiro(e, NULL);
+        mostrar_tabuleiro(e, NULL);
     }
     num_comandos++;
     return interpretador(e, num_comandos, state);
 }
 
-void imprimir_tabuleiro(ESTADO *e, FILE *file){
-    for(int row = 7; row >=0; row--){
-        for(int col = 0; col < 8; col++){
-            if(file == NULL) putchar(obter_casa(e, row, col));
-            else putc(obter_casa(e, row, col), file);
-        }
-        if(file == NULL) putchar('\n');
-        else putc('\n', file);
-    }
+void imprimir_char(int c, FILE *file){
+    if(file == NULL) putchar(c);
+    else putc(c, file);
 }
 
 void imprime_movs1(FILE *file, int i, int c1, int r1, int c2, int r2){
