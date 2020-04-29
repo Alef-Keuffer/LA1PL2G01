408f4df8cc00e380474f2a56c2f641a4ea99f8aa
Pedro Paulo Costa Pereira <62028225+pedrocpereira@users.noreply.github.com>
2020-04-29 15:22:02 +0100

    Novo módulo.

---
+++ Camadas/logica/auxiliares.h
@@ -0,0 +1,18 @@
+#ifndef RASTROS_AUXILIARES_H
+#define RASTROS_AUXILIARES_H
+#include <math.h>
+#include "dados.h"
+#include "modificar_estado.h"
+#include "acessar_estado.h"
+#include "logica.h"
+
+void ler_linha(ESTADO *e, int linha, const char *line);
+void ler_jogada(ESTADO *e, JOGADA jog, int numjog);
+void ler_restante(ESTADO *e, int jogadas, COORDENADA c);
+int coord_to_int(COORDENADA c);
+COORDENADA int_to_coord(int x);
+int avaliar_vitoria(ESTADO *e);
+int array_jogadaspossiveis(ESTADO *e, COORDENADA c, COORDENADA moveList[]);
+COORDENADA maisproximaFim(ESTADO *e, COORDENADA moveList[], int moveCount);
+
+#endif //RASTROS_AUXILIARES_H
